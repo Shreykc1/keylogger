23155032cbe111a953b28d716f430de4e1e799e8
unknown <Admin@MUM15115CPU1211.SVKMGRP.COM>
2024-07-03 09:16:55 +0530

    otp

--- Cipher/otp.py
+++ Cipher/otp.py
@@ -21,10 +21,29 @@ def otpE(key,pt):
     return otp
 
 
-      
+def otpD(key,ct):
+    ctNum = []
+    keyNum = []
+    sum = []
     
+    ct = ct.lower()
+    key = key.lower()
+    for i in ct:
+         ctVal = abs((ord(i) - 97))
+         ctNum.append(ctVal)
+    for i in key:
+         keyVal = abs((ord(i) - 97))
+         keyNum.append(keyVal)
     
+    for i in range(len(ctNum)):
+        sum.append((ctNum[i] - keyNum[i]) % 26)
+    
+    otp = ''
+    for i in sum:
+         otp += chr(i + 97)
+    return otp
 
 
-
-print(otpE("MONEY","HELLO"))
+encryt = otpE("MONEY","HELLO")
+print (f'Encrypted Text : {encryt}')
+print(f'Decrypted Text : {otpD("MONEY",otpE("MONEY","HELLO"))}')
